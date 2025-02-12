319de8b9e5ff4b43a86f96e3d1ec771237bfd66d
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-02-12 07:42:03 +0100

    Update stdlib.h

--- src/stdlib/stdlib.h
+++ src/stdlib/stdlib.h
@@ -5,11 +5,4 @@
 
 void* memset(void *ptr, int, size_t);
 
-
-
-void* malloc(size_t);   // allocazione dinamica della memoria
-void* calloc(size_t);   // allocazione dinamica della memoria ma con tutti gli elementi inizializzati a 0
-
-
-
 #endif
