b64544892da8c867f3daa2db84b19005361eb946
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-02-12 07:41:35 +0100

    Update stdlib.c

--- src/stdlib/stdlib.c
+++ src/stdlib/stdlib.c
@@ -1,8 +1,5 @@
 #include "stdlib/stdlib.h"
 
-void* current_address = (void*) (0x1000000);
-
-
 void* memset(void *ptr, int c, size_t n)
 {
     char *ptr_t = (char*) ptr;
@@ -13,22 +10,3 @@ void* memset(void *ptr, int c, size_t n)
 
     return ptr;
 }
-
-
-
-void* malloc(size_t size)
-{
-    void *tmp = current_address;
-    current_address += size;
-    return tmp;
-}
-
-
-void* calloc(size_t size)
-{
-    void *tmp = current_address;
-    current_address += size;
-    return memset(tmp, 0, size);
-}
-
-
