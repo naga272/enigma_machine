1ad9923b09eecce8617584605f055b945513cf66
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-04-20 10:31:40 +0200

    Delete src directory

--- src/string/string.c
+++
@@ -1,9 +0,0 @@
-#include "string/string.h"
-
-unsigned char* strcat(unsigned char* ptr1, size_t size_ptr1, unsigned char* ptr2)
-{
-    for (size_t i = 0; i < size_ptr1; i++)
-        ptr1[i] = ptr2[i];
-    
-    return ptr1;
-}
--- src/string/string.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRING_H
-#define STRING_H
-
-#include <stddef.h>
-unsigned char* strcat(unsigned char* ptr1, size_t size_ptr1, unsigned char* ptr2);
-
-#endif
