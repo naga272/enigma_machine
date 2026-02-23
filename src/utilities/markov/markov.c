27a1e3cdeec66b41c816c48bbeb79ce73679acf0
naga272 <bastianellofederico4@gmail.com>
2026-02-23 05:51:21 +0100

    Ridotto cache miss

--- src/utilities/markov/markov.c
+++ src/utilities/markov/markov.c
@@ -74,8 +74,8 @@ O3 static inline void panic_table_prediction_idt()
     *   cosi non parto proprio da 0 di nuovo ma ho comunque
     *   una base da predire
     * * */
-    for (u8 x = 0; x < ACTUAL_INTRT_CREATED; x++)
-        for (u8 y = 0; y < ACTUAL_INTRT_CREATED; y++)
+    for (u8 y = 0; y < ACTUAL_INTRT_CREATED; y++)    
+        for (u8 x = 0; x < ACTUAL_INTRT_CREATED; x++)
             markov_entry[y][x] /= 2;
 }
 
